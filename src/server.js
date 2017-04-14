9043a7f0901cf1f8d4e9c2f005ea08f76799aadb
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-04-14 11:55:05 +0100

    initial file set up initial file set up with Hapi, serves a static file, also set up eslint

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const hapi = require('hapi');
+
+const server = new hapi.Server();
+const inert = require('inert');
+const vision = require('vision');
+const routes = require('./routes');
+// const handlebars = require('./handlebars');
+
+server.connection({
+  host: 'localhost',
+  port: 4000,
+});
+
+server.register([inert, vision], (err) => {
+  if (err) throw err;
+
+  // server.views(handlebars);
+  server.route(routes);
+});
+
+module.exports = server;
